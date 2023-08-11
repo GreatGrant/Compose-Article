302fba39dbd8a075494aefd1ab638705dab942db
Great Grant <greatgrant116@gmail.com>
2023-08-11 10:02:59 +0100

    complete card

--- app/src/main/java/com/gralliams/composearticle/MainActivity.kt
+++ app/src/main/java/com/gralliams/composearticle/MainActivity.kt
@@ -3,13 +3,22 @@ package com.gralliams.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gralliams.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +31,58 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ComposeArticleCard()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ComposeArticleCard(modifier : Modifier = Modifier) {
+        Column(
+            modifier = modifier,
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.bg_compose_background),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth
+            )
+
+            Text(
+                text = stringResource(R.string.header_text),
+                fontSize = 24.sp,
+                modifier = modifier
+                    .padding(16.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.body_text1),
+                textAlign = TextAlign.Justify,
+                        modifier = modifier
+                    .padding(start = 16.dp, end = 16.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.body_text2),
+                textAlign = TextAlign.Justify,
+                modifier = modifier.padding(16.dp)
+            )
+
+
+        }
+
+
 }
 
-@Preview(showBackground = true)
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun ArticlePreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        ComposeArticleCard()
     }
 }
